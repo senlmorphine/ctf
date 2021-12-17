e2d3931ebf34a0baff3c164dc2a59e19e3891801
senlmorphine <96277522+senlmorphine@users.noreply.github.com>
2021-12-17 15:30:32 +0100

    Delete neutron directory

--- neutron/challenge/app/preload.js
+++
@@ -1,7 +0,0 @@
-const { ipcRenderer, contextBridge } = require('electron')
-
-contextBridge.exposeInMainWorld('electron',
-  {
-    safeEval: (input) => ipcRenderer.invoke('safeEval', input)
-  }
-)
