60ba2a0d691c9c7761a77bcab9788639042a5d38
senlmorphine <96277522+senlmorphine@users.noreply.github.com>
2021-12-18 20:04:55 +0100

    Delete sweatin-in-latex directory

--- sweatin-in-latex/source/cleanpdfdir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR=/var/www/html/pdf/
-SIZE=$(du -sm $DIR | grep -oP "\d+")
-if [[ $SIZE -gt 100 ]]; then
-	rm -r $DIR*.pdf
-fi
--- sweatin-in-latex/source/public_html/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$PDFLATEX = "/usr/bin/pdflatex";
-$COMPILEDIR = __DIR__ . "/compile/";
-$OUTPUTDIR = __DIR__ . "/pdf/";
-$DLURL = "http://" . $_SERVER['SERVER_NAME'] . ":" . $_SERVER['SERVER_PORT'] . "/pdf/";
-?>
