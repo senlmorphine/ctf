caea16a8a9acf84c2eb91986bc96ba1008531ffe
senlmorphine <96277522+senlmorphine@users.noreply.github.com>
2022-05-25 07:03:41 -0400

    Delete analects directory

--- analects/mysql/init/3-flag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /docker-entrypoint-initdb.d
-
-if [ -f flag.txt ]; then
-  read flag < flag.txt
-  mysql -u root -p"$MYSQL_ROOT_PASSWORD" analects -e "UPDATE flag SET flag='$flag';"
-fi
